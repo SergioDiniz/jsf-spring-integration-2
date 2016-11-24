fdb181fb7cf6a6eb1466cfca8b4488de3c733ee3
Sergio Diniz <sergiodinizsh@gmail.com>
2016-11-24 10:27:33 -0300

    file upload

--- src/br/com/controllers/UserController.java
+++ src/br/com/controllers/UserController.java
@@ -1,11 +1,16 @@
 package br.com.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
 
+import org.primefaces.model.UploadedFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,7 +26,7 @@ public class UserController{
 	UserService userService;
 
 	private User user;
-	
+	private UploadedFile file;
 	
 	public UserController() {
 		// TODO Auto-generated constructor stub
@@ -29,9 +34,29 @@ public class UserController{
 	}
 
 	
-	public void addUser(){
+	public void addUser() throws IOException{
+		
+		File root = new File("/Users/sergiodiniz/uploads/");
+		String nomeArquivo = this.file.getFileName();
+		String formatoArquivo = nomeArquivo.substring(nomeArquivo.lastIndexOf("."), nomeArquivo.length());
+		nomeArquivo = System.currentTimeMillis() + formatoArquivo;
+		
+		
+		FileInputStream input = (FileInputStream) file.getInputstream();
+		FileOutputStream out = new FileOutputStream(new File(root, nomeArquivo));
+		
+		byte[] buffer = new byte[(int) file.getSize()];
+		int contador = 0;
+		while((contador = input.read(buffer)) != -1){
+			out.write(buffer, 0, contador);
+		}
+		input.close();
+		out.close();
+		
+		this.user.setArquivo(nomeArquivo);
+		this.user.setTipoArquivo(formatoArquivo);
 		System.out.println(this.user.getNome());
-		userService.add(new User());
+		userService.add(this.user);
 		this.user = new User();
 	}
 	
@@ -41,6 +66,16 @@ public class UserController{
 
 	public void setUser(User user) {
 		this.user = user;
+	}
+
+
+	public UploadedFile getFile() {
+		return file;
+	}
+
+
+	public void setFile(UploadedFile file) {
+		this.file = file;
 	}
 	
 	
